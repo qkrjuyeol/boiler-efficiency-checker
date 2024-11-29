e765d55d6f2e54788865fc99bbdb5e20759e5ade
jdh7351 <jdh7351@naver.com>
2024-11-29 22:13:03 +0900

    전처리 api 추가

--- back/app.py
+++ back/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 from werkzeug.serving import WSGIRequestHandler
-
+import base64
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -21,14 +23,20 @@ def api_model_DB():
     WSGIRequestHandler.timeout = 100000
     cursor = mysql.connection.cursor()
     query = "INSERT INTO ai (name, data) VALUES (%s, %s)"
-    file_name = ['random_forest.pkl', 'reduced_random_forest.pkl', 'scaler.pkl']
+    file_name = ['random_forest.pkl', 'reduced_random_forest.pkl', 'scaler.pkl', 'differential_evolution.pkl']
     for i in range(1,3):
-        binary_data = open(file_name[0], 'rb').read()
-        cursor.execute(query, (file_name[0], binary_data))
+        binary_data = open(file_name[i], 'rb').read()
+        cursor.execute(query, (file_name[i], binary_data))
     mysql.connection.commit()
     cursor.close()
     return jsonify({'message': 'Model has been saved to the database'})
 
+@app.route('/preprocessing', methods=['GET'])
+def img_preprocessing():
+    with open('preprocessing_img.jpg', 'rb') as img_file:
+        img_data = img_file.read()
+        img_base64 = base64.b64encode(img_data).decode('utf-8')
+    return jsonify({'name': 'data.jpg', 'data': img_base64})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
